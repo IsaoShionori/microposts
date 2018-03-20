598f1e0785dee9ff23e47300c51bda45fa889d7b
EC2 Default User <ec2-user@ip-172-31-29-148.ap-southeast-1.compute.internal>
2018-03-20 23:33:36 +0000

    add follow & unfollwo, and modified login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only:[:index, :show]
+  before_action :require_user_logged_in, only:[:index, :show, :followings, :followers]
   
   def index
     @users = User.all.page(params[:page])
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.order('created_at DESC').page(params[:page])
-    counts(user)
+    counts(@user)
   end
 
   def new
@@ -19,19 +19,31 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
-      flash[:success] = ''
+      flash[:success] = 'ユーザを登録しました'
       redirect_to @user
     else
-      flash.now[:danger] = ''
+      flash.now[:danger] = 'ユーザの登録に失敗しました'
       render :new
     end
+  end
+  
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
   end
 
   
   private
   
   def user_params
-    params.require(:user).permit(:name, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
